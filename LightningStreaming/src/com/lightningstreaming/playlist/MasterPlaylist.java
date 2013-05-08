a0465d731485def98c4ff157060908e03cead35b
Alberto Montes <alm59321@gmail.com>
2013-05-08 16:31:49 +0200

    AM

--- LightningStreaming/src/com/lightningstreaming/playlist/MasterPlaylist.java
+++ LightningStreaming/src/com/lightningstreaming/playlist/MasterPlaylist.java
@@ -1,18 +1,119 @@
 package com.lightningstreaming.playlist;
 
 import java.io.File;
+import java.net.URI;
 import java.net.URL;
+import java.util.Dictionary;
 
-public class MasterPlaylist extends Playlist {
+public class MasterPlaylist {
 
-	public MasterPlaylist(float targetDuration, int mediaSequence) {
-		super(targetDuration, mediaSequence);
+	private float totalDuration;
+	private int numSegments;
+	
+
+	private String name;
+	private URI path;
+	private URL url;
+	private int numStreams;
+	
+	private Dictionary<Integer, SegmentPlaylist> streams;
+	
+	public MasterPlaylist (String name, URI path, URL url, Dictionary<Integer, SegmentPlaylist> streams) {
+		this.setName(name);
+		this.setPath(path);
+		this.setUrl(url);
+		this.setStreams(streams);
+		this.setNumStreams(streams.size());
 	}
 
-	@Override
-	public Playlist parse(File file, URL url) {
-		// TODO Auto-generated method stub
+	public static MasterPlaylist parse(File file, URL url) {
+		
+		String n = file.getName();
+		URI p = new URI(file.getAbsolutePath());
+		
+		
+		
 		return null;
+	}
+
+
+
+	public float getTotalDuration() {
+		return totalDuration;
+	}
+
+
+
+	public void setTotalDuration(float totalDuration) {
+		this.totalDuration = totalDuration;
+	}
+
+
+
+	public int getNumSegments() {
+		return numSegments;
+	}
+
+
+
+	public void setNumSegments(int numSegments) {
+		this.numSegments = numSegments;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public URI getPath() {
+		return path;
+	}
+
+
+
+	public void setPath(URI path) {
+		this.path = path;
+	}
+
+
+
+	public URL getUrl() {
+		return url;
+	}
+
+
+
+	public void setUrl(URL url) {
+		this.url = url;
+	}
+
+
+
+	public Dictionary<Integer, SegmentPlaylist> getStreams() {
+		return streams;
+	}
+
+
+
+	public void setStreams(Dictionary<Integer, SegmentPlaylist> streams) {
+		this.streams = streams;
+	}
+
+	public int getNumStreams() {
+		return numStreams;
+	}
+
+	public void setNumStreams(int numStreams) {
+		this.numStreams = numStreams;
 	}
 
 }
--- LightningStreaming/src/com/lightningstreaming/playlist/SegmentPlaylist.java
+++ LightningStreaming/src/com/lightningstreaming/playlist/SegmentPlaylist.java
@@ -3,26 +3,127 @@ package com.lightningstreaming.playlist;
 import java.io.File;
 import java.net.URI;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Vector;
 
-public class SegmentPlaylist extends Playlist {
+import com.lightningstreaming.regex.Regex;
+
+
+
+public class SegmentPlaylist {
 	
-	String tag;
-	URI relativePath;
+	private String tag;
+	private URL url;
+	private URI relativePath;
+	private int mediaSequence;
+	private float targetDuration;
+	
 	List<Segment> segments;
 	
 	public SegmentPlaylist(float targetDuration, int mediaSequence, URL url, String tag, List<Segment> segments) {
-		super(targetDuration, mediaSequence);
-		super.setNumSegments(segments.size());
-		this.tag = tag;
+		this.setTargetDuration(targetDuration);
+		this.setMediaSequence(mediaSequence);
+		this.setUrl(url);
+		this.setTag(tag);
+		this.segments = segments;
 		
 		
 		// Condicionals for extrange values of targetduration with throws
 	}
 
-	@Override
-	public Playlist parse(File file, URL url) {
-		return null;
+	public int getNumSegments() {
+		return this.segments.size();
+	}
+	
+	public static SegmentPlaylist parse(File file, URL url) {
+		
+		String data = com.lightningstreaming.regex.Regex.fileToString(file);
+		
+		float t = -1;
+		int m = -1;
+		
+		if (data.contains("#EXT-X-TARGETDURATION")) {
+			t = Float.parseFloat(Regex.extractString(data, "#EXT-X-TARGETDURATION:", "\n"));
+		}
+		
+		if (data.contains("#EXT-X-MEDIA-SEQUENCE")) {
+			m = Integer.parseInt(Regex.extractString(data, "#EXT-X-MEDIA-SEQUENCE:", "\n"));
+		}
+		
+		String tag = file.getName();
+		
+		List<Segment> s = new ArrayList<Segment>();
+		int numSegments = Regex.count(data, "#EXTINF:");
+		if (numSegments > 0) {
+			Vector<String> seg = new Vector<String>(Arrays.asList(data.split("#EXTINF")));
+			seg.remove(0);
+			for (int i = 0; i < numSegments; i++) {
+				float dur = Float.parseFloat(Regex.extractString(seg.get(i), ":", ","));
+				URL urlSegment = null;
+				URI rPath = null;
+				//String u;
+				try {
+					
+					urlSegment = new URL(Regex.extractString(seg.get(i), "\n", "\n"));
+					//u = Regex.extractString(seg.get(i), "\n", "\n");
+					
+					if (Regex.count(urlSegment.toString(), "/") > 0) 
+						rPath = url.toURI().relativize(urlSegment.toURI());
+					else
+						rPath = new URI(urlSegment.toString());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+				s.add(new Segment (i, dur, rPath, urlSegment));
+			}
+		}
+		
+		SegmentPlaylist playlist = new SegmentPlaylist (t, m, url, tag, s);
+		
+		return playlist;
+	}
+
+	public int getMediaSequence() {
+		return mediaSequence;
+	}
+
+	public void setMediaSequence(int mediaSequence) {
+		this.mediaSequence = mediaSequence;
+	}
+
+	public float getTargetDuration() {
+		return targetDuration;
+	}
+
+	public void setTargetDuration(float targetDuration) {
+		this.targetDuration = targetDuration;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	public URI getRelativePath() {
+		return relativePath;
+	}
+
+	public void setRelativePath(URI relativePath) {
+		this.relativePath = relativePath;
+	}
+
+	public URL getUrl() {
+		return url;
+	}
+
+	public void setUrl(URL url) {
+		this.url = url;
 	}
 	
 }
